6940a75f526fc0883090c41f8c8063ac4fe6bc60
luna <93258461+LunaDEV-net@users.noreply.github.com>
2024-11-01 17:39:46 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import file
 import const
 from data import process_data
 
+__version__ = "2024-11-1_v1.9"
 
 def main(path_in: pathlib.Path, path_out: pathlib.Path):
     file_contents = file.read_csv_file(path_in)
@@ -13,7 +14,12 @@ def main(path_in: pathlib.Path, path_out: pathlib.Path):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Process some CSV files.")
+    parser = argparse.ArgumentParser(
+        description="A local python tool to turn raw data from the Aktion Saubere Hände into an excel friendly dataset.",
+        epilog="https://github.com/LunaDEV-net/WebKess \n Support: 
+Contact me personaly or write a [Github Issue](https://github.com/LunaDEV-net/WebKess/issues)"
+    )
+    parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('path_in', type=pathlib.Path, help='Input CSV file path')
     parser.add_argument('path_out', type=pathlib.Path, help='Output CSV file path')
 
