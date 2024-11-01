eb505c65307a94fbbdedb2d38f86b54a2847cc41
luna <93258461+LunaDEV-net@users.noreply.github.com>
2024-11-01 18:17:35 +0100

    Update main.py
    nice to have features

--- src/main.py
+++ src/main.py
@@ -16,12 +16,14 @@ def main(path_in: pathlib.Path, path_out: pathlib.Path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="A local python tool to turn raw data from the Aktion Saubere Hände into an excel friendly dataset.",
-        epilog="https://github.com/LunaDEV-net/WebKess \n Support: 
-Contact me personaly or write a [Github Issue](https://github.com/LunaDEV-net/WebKess/issues)"
+        epilog=r'https://github.com/LunaDEV-net/WebKess  Support: Contact me personaly or write a [Github Issue](https://github.com/LunaDEV-net/WebKess/issues)'
     )
     parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('path_in', type=pathlib.Path, help='Input CSV file path')
     parser.add_argument('path_out', type=pathlib.Path, help='Output CSV file path')
 
-    args = parser.parse_args()
-    main(args.path_in, args.path_out)
+    try:
+        args = parser.parse_args()
+        main(args.path_in, args.path_out)
+    except SystemExit:
+        parser.print_help()
