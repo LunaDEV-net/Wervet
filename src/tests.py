06abdbae9487ea480ae67caacb192db25c7d9c4c
LunaDEV-net <93258461+LunaDEV-net@users.noreply.github.com>
2024-11-03 20:50:30 +0100

    did #5

--- src/tests.py
+++ src/tests.py
@@ -7,18 +7,22 @@ def check_data(data_in: list, const_data: Default):
     # Test 1
     if len(data_in) == 0:
         sys.exit("Test 1 failed: No data found")
-    for line_num in range(len(data_in)):
+    for line_num, line in enumerate(data_in):
         # # Test 2
         # if len(data_in[line_num]) != const_data.number_of_columns:
         #     sys.exit(f"Test 2 failed: Data format of CSV file changed and is not supported! \n {len(data_in[line_num])} in line {line_num} != {const_data.number_of_columns}")
         # Hat immer Fehler bei von mir bearbeiten CSV Files gemacht
         # Test 3
         for i in const_data.should_not_be_empty_columns:
-            if data_in[line_num][i] == "":
+            if line[i] == "":
                 sys.exit(f"Test 3 failed: Empty column found in line {line_num}")
-        for columns in data_in[line_num]:
+        # Test 4
+        for columns in line:
             if any(char in columns for char in const_data.disallowed_characters):
                 sys.exit(f"Test 4 failed: Disallowed character ({const_data.disallowed_characters}) found in line {line_num}")
+        # Test 5
+        if line[const_data.indikator] not in const_data.indexe_indikatoren:
+            sys.exit(f"Test 5 failed: Indikator not found in indexe_indikatoren in line {line_num}")
 
 
 
