1aefee7094a03e6278d9f3a49f65cd3815a1a9b2
A windows User <windows@invalid.invalid>
2024-11-14 20:52:42 +0100

    moved and adaptered .bat scripts except run.bat to Scripts folder

---
+++ Scripts/updater.bat
@@ -0,0 +1,11 @@
+@echo off
+rem check if path is correct (only the explorer)
+rem https://stackoverflow.com/a/32423352
+for %%a in ("%~dp0\.") do set "parent=%%~nxa"
+if %parent%==Scripts echo switch to partent dir
+if %parent%==Scripts cd ..
+rem should the in WebKess Folder
+
+git pull
+echo success
+PAUSE
